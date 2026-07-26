c8091d2f967572b02eb86190a2c8fecb65de1d6b
Y-K-SAI-SRIKAR <saisrikaryerraguntla@gmail.com>
2026-07-26 23:09:58 +0530

    Numpy Project Day 01 Status

---
+++ DataSet.py
@@ -0,0 +1,53 @@
+# Numpy Mini Project:
+# Data Set Creating :
+import numpy as np
+
+#data set cols index:
+ROLL = 0
+MATH = 1
+PYTHON = 2
+DSA = 3
+OOP = 4
+SYSTEM_DESIGN = 5
+ATTENDANCE = 6
+
+#data set cols:
+headers = np.array([
+    "RollNo",
+    "Math",
+    "Python",
+    "DSA",
+    "OOP",
+    "SystemDesign",
+    "Attendance"
+])
+
+np.random.seed(42)
+
+#data set rows:
+roll_no = np.arange(1,101)
+math_marks = np.random.randint(0,101,100)
+python_marks = np.random.randint(0,101,100)
+dsa_marks = np.random.randint(0,101,100)
+oop_marks = np.random.randint(0,101,100)
+system_design_marks = np.random.randint(0,101,100)
+attendance = np.random.randint(60,101,100)
+
+#Combined Data Set:
+StudentsDataSet = np.column_stack((roll_no,
+                                   math_marks,
+                                   python_marks,
+                                   dsa_marks,
+                                   oop_marks,
+                                   system_design_marks,
+                                   attendance))
+
+#saving Data as npy file:
+np.save('StudentData.npy',StudentsDataSet)
+print(".Npy file Created Successfully")
+
+#saving as a zip file:
+np.savez('StudentData.npz',index=headers,data=StudentsDataSet)
+print(".Npz file Created Successfully")
+
+#<<<<<<<< ------- created on 26-07-2026 @ 23:00 ------- >>>>>>>> < Author : Srikar Yerraguntla >
